f1689459c3aeb3274dafd960736a1d2d77747a3a
RushikeshPujari <rushikeshanilpujari@gmail.com>
2019-03-23 09:48:23 +0530

    first commit

---
+++ precache-manifest.4199dbd3d41444bd76e4bb6039aeaae7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "e56057e5fe4a66855a4e",
+    "url": "/js/chunk-vendors.7f67c001.js"
+  },
+  {
+    "revision": "3535918bf5198843ff3d",
+    "url": "/js/app.ee48082c.js"
+  },
+  {
+    "revision": "116f85b2c5f66290a8db835be5cac152",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e56057e5fe4a66855a4e",
+    "url": "/css/chunk-vendors.fcfc8af4.css"
+  },
+  {
+    "revision": "3535918bf5198843ff3d",
+    "url": "/css/app.db66266f.css"
+  },
+  {
+    "revision": "a2e691c197f11dcfaf90b9b8a048e34b",
+    "url": "/avtar.png"
+  }
+];
